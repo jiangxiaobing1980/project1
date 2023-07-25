237b4882e2c6d0e801784f276139758621121972
jiangxiaobing1980 <59365868+jiangxiaobing1980@users.noreply.github.com>
2023-07-25 15:52:34 +0800

    0725-1552Update EPDM.h

--- src/EPDM.h
+++ src/EPDM.h
@@ -9,17 +9,24 @@
 2.返回单价
 3.返回金额
 */
+class Group;
 class EPDM
 {
 public:
 	EPDM();
 	virtual ~EPDM();
 public:
+	double exitdistance;
 	double m_weight;
 	double m_price;
 
 public:
-	EPDM(double weight, double price) :m_weight(weight)， m_price(price) {};
+	EPDM( Group* pgroup , double distance, double weight, double price) :exitdistance(distance) ,m_weight(weight), m_price(price) {};
+
+	double getLenght(){return 0 ; };	//pgroup(宽+高)*2-exitdistance*8.0
+	double getWeight(){return getLenght() * m_weight ;};
+	double getAmount(){return getLenght() * m_price};
+		   
 
 
 };
